ac66fa2e4baa17c7ebd002a17ca61c56b106abda
Alisson Lee Martins <alissonleev2@gmail.com>
2025-04-15 09:10:12 -0300

    inclusão de seção de visualização de tarefas e lógica de tarefa realizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor full stack",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar guitarra",
+      description: "Estudar guitarra para se tornar um guitarrista muito bom",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Ir ao mercado",
+      description: "Comprar arroz, feijão, macarrão e sorvete",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id == taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
 
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <AddTask />
-      <Tasks />
+    <div className="w-screnn h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,28 @@
-function Tasks() {
-    return <h1>Tarefas</h1>
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
+function Tasks(props) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-white p-2 rounded-md w-full ${
+              task.isCompleted && "line-through bg-green-300"
+            }`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <TrashIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
